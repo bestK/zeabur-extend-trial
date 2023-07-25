c98e97861722309c3b3d6d76b32ea70f0a938e10
ECH2O <1138493417@qq.com>
2023-07-25 05:24:24 +0000

    fix

--- checkin.sh
+++ checkin.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export $(cat /proc/1/environ |tr '\0' '\n' | xargs)
+
 /usr/local/bin/python /app/zeabur_extend_trial/main.py >> /app/cron.log 2>&1
